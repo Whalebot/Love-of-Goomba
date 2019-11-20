4636f1f43ab383d9f1bd65046b05c12f1ac4d4aa
Whale <mitr1659@edu.kadk.dk>
2019-11-20 19:21:24 +0100

    Basic navigation and enemies

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class AI : MonoBehaviour
 {
+    public GameObject target;
+    NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (target == null) target = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+       
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        agent.SetDestination(target.transform.position);
     }
 }
+
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public bool isRanged;
+    [SerializeField] GameObject projectile;
+    [SerializeField] Transform shootOrigin;
+    [SerializeField] float range;
+    [SerializeField] LayerMask mask;
+    [SerializeField] float shootDelay;
+    [SerializeField] float lastShot;
+    RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isRanged)
+        {
+            ShootDetection();
+        }
+    }
+
+    void ShootDetection()
+    {
+        Debug.DrawLine(transform.position, transform.position + transform.forward * range, Color.blue);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range, mask))
+        {
+            if (Time.time > shootDelay + lastShot)
+            {
+                lastShot = Time.time;
+                Shoot();
+            }
+        }
+    }
+
+    public void Shoot()
+    {
+        Instantiate(projectile, shootOrigin.position, shootOrigin.rotation);
+    }
+}
