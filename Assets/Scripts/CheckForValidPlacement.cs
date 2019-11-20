4ef9582d16f3424153df8b8e2850b54b4633a744
Janek <amok.skull@gmail.com>
2019-11-20 20:49:08 +0100

    Game Master UI Prototype

---
+++ Assets/Scripts/CheckForValidPlacement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForValidPlacement : MonoBehaviour
+{
+     public bool playerInBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInBounds = false;
+    }
+
+    void OnTriggerStay(Collider other){
+        if(other.gameObject.tag == "ValidPlacementTag"){
+            playerInBounds = true;
+        }
+    }
+    
+    void OnTriggerExit(Collider other){
+        if(other.gameObject.tag == "ValidPlacementTag"){
+            playerInBounds = false;
+        }
+    }
+}
