e42e72314572005fd580caec0d0e96a407b690ca
jesperqwop <jespertuan@hotmail.com>
2019-11-23 19:33:29 +0100

    small stuff

---
+++ Assets/Scripts/Jesper Scripts/PlaytestUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaytestUI : MonoBehaviour
+{
+    Text text;
+    Status status;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        status = GameObject.FindGameObjectWithTag("Player").GetComponent<Status>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = status.health.ToString();
+    }
+}
