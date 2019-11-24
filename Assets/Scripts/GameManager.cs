0b1bfcb6e69f94fa4da22c125d2a6d911c127f2b
Whale <mitr1659@edu.kadk.dk>
2019-11-24 07:19:39 +0100

    Updated Michael Scene with Ling stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public GameObject GMBase;
     public int GMMoney;
+    public int income;
     [SerializeField] float timeCounter;
     // Start is called before the first frame update
     void Start()
