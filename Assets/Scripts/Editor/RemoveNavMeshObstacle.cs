9990a063c9285367d25aa8e6194f5c30dd60b058
Whale <mitr1659@edu.kadk.dk>
2019-11-27 07:23:24 +0100

    Pre delete nav mesh obstacles

---
+++ Assets/Scripts/Editor/RemoveNavMeshObstacle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RemoveComponent))]
+public class RemoveNavMeshObstacle : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        RemoveComponent rmc = (RemoveComponent)target;
+
+        if (GUILayout.Button("Remove ComponentName"))
+        {
+            rmc.RemoveComponents();
+        }
+    }
+}
